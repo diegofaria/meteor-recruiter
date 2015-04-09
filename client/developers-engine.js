9b591b20a5fc8440298b203a932b3b14f07ec954
diegofaria <diegofndefaria@gmail.com>
2015-04-08 22:22:35 -0300

    added mobile, back-end and unqualified rankings
    - created a uncoupled engine
    - some refactorings and method extractions

---
+++ client/developers-engine.js
@@ -0,0 +1,92 @@
+CandidatesEngine = {
+	getFrontEndDevelopers : function() {
+		var allDevelopers = Candidates.find({}).fetch();
+		var developers = []
+		var _this = this;
+		_.each(allDevelopers, function(developer) {
+			var developer = _this.buildTemplateObject(developer);
+			if (_this.isFrontEnd(developer)) {
+				developers.push(developer);
+			}
+		})
+		return developers;
+	},
+
+	getBackEndDevelopers : function() {
+		var allDevelopers = Candidates.find({}).fetch();
+		var developers = []
+		var _this = this;
+		_.each(allDevelopers, function(developer) {
+			var developer = _this.buildTemplateObject(developer);
+			if (_this.isBackEnd(developer)) {
+				developers.push(developer);
+			}
+		})
+		return developers;
+	},
+	getMobileDevelopers : function() {
+		var allDevelopers = Candidates.find({}).fetch();
+		var developers = []
+		var _this = this;
+		_.each(allDevelopers, function(developer) {
+			var developer = _this.buildTemplateObject(developer);
+			if (_this.isMobile(developer)) {
+				developers.push(developer);
+			}
+		})
+		return developers;
+	},
+	getUnqualifiedDevelopers : function() {
+		var allDevelopers = Candidates.find({}).fetch();
+		var developers = []
+		var _this = this;
+		_.each(allDevelopers, function(developer) {
+			var developer = _this.buildTemplateObject(developer);
+			if (_this.isUnqualified(developer)) {
+				developers.push(developer);
+			}
+		})
+		return developers;
+	},
+
+	getAllDevelopers : function() {
+		var allDevelopers = Candidates.find({}).fetch();
+		var developers = []
+		var _this = this;
+		_.each(allDevelopers, function(developer) {
+			var developer = _this.buildTemplateObject(developer);
+			developers.push(developer);
+		})
+		return developers;
+	},
+
+	buildTemplateObject : function(developer) {
+		return {
+			name: developer.name,
+			email: developer.email,
+			html: developer.skills['front-end'].html,
+			css: developer.skills['front-end'].css,
+			javascript: developer.skills['front-end'].javascript,
+			python: developer.skills['back-end'].python,
+			django: developer.skills['back-end'].django,
+			ios: developer.skills['mobile'].ios,
+			android: developer.skills['mobile'].android
+		}
+	},
+
+	isFrontEnd : function (developer) {
+		return developer.html >= 7 && developer.css >= 7 && developer.javascript >= 7
+	},
+
+	isBackEnd : function (developer) {
+		return developer.python >= 7 && developer.django >= 7
+	},
+
+	isMobile : function (developer) {
+		return developer.ios >= 7 && developer.android >= 7
+	},
+
+	isUnqualified : function (developer) {
+		return !this.isFrontEnd() && !this.isBackEnd() && !this.isMobile()
+	}
+}
--- client/views/ranking.js
+++ client/views/ranking.js
@@ -1,27 +1,22 @@
 Router.route('ranking', {
 	path: '/ranking',
 	template: 'ranking',
-})
+});
 
 Template.ranking.helpers({
 	"frontEndDevelopers": function() {
-		var allDevelopers = Candidates.find({}).fetch();
-		var frontEndDevelopers = []
-		_.each(allDevelopers, function(developer) {
-			var html = developer.skills['front-end'].html;
-			var css = developer.skills['front-end'].css;
-			var javascript = developer.skills['front-end'].javascript;
-			if (html >= 7) {
-				var frontEndDeveloper = {
-					name: developer.name,
-					email: developer.email,
-					html: html,
-					css: css,
-					javascript: javascript
-				};
-				frontEndDevelopers.push(frontEndDeveloper);
-			}
-		})
-		return frontEndDevelopers;
+		return CandidatesEngine.getFrontEndDevelopers();
+	},
+	"backEndDevelopers": function() {
+		return CandidatesEngine.getBackEndDevelopers();
+	},
+	"mobileDevelopers": function() {
+		return CandidatesEngine.getMobileDevelopers();
+	},
+	"unqualifiedDevelopers": function() {
+		return CandidatesEngine.getUnqualifiedDevelopers();
+	},
+	"allDevelopers": function() {
+		return CandidatesEngine.getAllDevelopers();
 	}
-})
+});
